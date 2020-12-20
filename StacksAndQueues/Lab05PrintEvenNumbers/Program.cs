78567b4dfbe43af465da77b39d0836653302ba11
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2020-12-20 18:44:29 +0200

    Add files via upload

---
+++ StacksAndQueues/Lab05PrintEvenNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Lab05PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> evenNums = new Queue<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(nums[i]%2==0)
+                {
+                    evenNums.Enqueue(nums[i]);
+                }
+            }
+
+           Console.WriteLine(string.Join(", ", evenNums));
+
+        }
+    }
+}
