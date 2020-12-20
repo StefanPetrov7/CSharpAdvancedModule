75c36c58f284d8aa37509a0501677a34aa0ec379
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2020-12-20 16:25:20 +0200

    Add files via upload

---
+++ StacksAndQueues/LabHotPotato/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LabHotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> kids = new Queue<string>(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray());
+            int count = int.Parse(Console.ReadLine());
+
+            while (kids.Count > 1 )
+            {
+                for (int i = 1; i < count; i++)
+                {
+                    kids.Enqueue(kids.Dequeue());
+                }
+                Console.WriteLine($"Remove {kids.Dequeue()}");
+            }
+
+            Console.WriteLine($"Last is {String.Join(" ",kids)}");
+        }
+    }
+}
