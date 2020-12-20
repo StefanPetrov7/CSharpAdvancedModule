2a8b9d4e7fe4071745efe88834402e23017d6251
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2020-12-20 19:01:16 +0200

    Add files via upload

---
+++ StacksAndQueues/Lab06Supermarket/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Lab06Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> people = new Queue<string>();
+
+            string input;
+
+            while ((input=Console.ReadLine())!="End")
+            {
+                if (input=="Paid")
+                {
+                    int num = people.Count;
+
+                    for (int i = 0; i < num; i++)
+                    {
+                        Console.WriteLine(people.Dequeue());
+                    }
+                }
+                else
+                {
+                    people.Enqueue(input);
+                }
+                
+            }
+
+            Console.WriteLine($"{people.Count} people remaining.");
+        }
+    }
+}
