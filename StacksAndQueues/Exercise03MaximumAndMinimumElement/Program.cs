c8f0b1bc5af680d1579e3176dc9bbfa912a2156c
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2020-12-21 20:00:54 +0200

    Add files via upload

---
+++ StacksAndQueues/Exercise03MaximumAndMinimumElement/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Exercise03MaximumAndMinimumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int queries = int.Parse(Console.ReadLine());
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < queries; i++)
+            {
+                int[] cmd = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                int command = cmd[0];
+
+                switch (command)
+                {
+                    case 1:
+                        stack.Push(cmd[1]);
+                        break;
+                    case 2:
+                        if (stack.Count>0)
+                        {
+                            stack.Pop();
+                        }
+                        break;
+                    case 3:
+                        if (stack.Count > 0)
+                        {
+                            Console.WriteLine(stack.Max());
+                        }
+                        break;
+                    case 4:
+                        if (stack.Count > 0)
+                        {
+                            Console.WriteLine(stack.Min());
+                        }
+                        break;
+                }
+            }
+
+            Stack<int> reverse = stack;
+
+            Console.WriteLine(string.Join(", ",reverse));
+
+        }
+    }
+}
