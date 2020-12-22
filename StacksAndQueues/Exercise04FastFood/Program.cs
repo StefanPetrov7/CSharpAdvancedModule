c9680102c4425fc3d524aa4b8c3ea61e1bc89f8d
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2020-12-22 15:26:15 +0200

    Add files via upload

---
+++ StacksAndQueues/Exercise04FastFood/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise04FastFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalFood = int.Parse(Console.ReadLine());
+            Queue<int> orders = new Queue<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+
+            int ordersCount = orders.Count;
+
+            Console.WriteLine(orders.Max());
+
+            for (int i = 0; i < ordersCount; i++)
+            {
+                if (totalFood < orders.Peek()) break;
+
+                int currentOrder = orders.Peek();
+                totalFood -= currentOrder;
+                orders.Dequeue();
+
+            }
+
+            if (orders.Count > 0)
+            {
+                Console.WriteLine("Orders left: " + string.Join(" ", orders));
+            }
+            else
+            {
+                Console.WriteLine("Orders complete");
+            }
+        }
+    }
+}
