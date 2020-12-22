ce97b037ba143fc28bebd6c84b18a5e0508e4595
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2020-12-22 16:54:16 +0200

    Add files via upload

---
+++ StacksAndQueues/Exercise06SongsQueue/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise06SongsQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> songsList = new Queue<string>(Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray());
+
+            while (songsList.Count>0)
+            {
+                string command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case"Play":
+                        songsList.Dequeue();
+                        break;
+                    case "Show":
+                        Console.WriteLine(string.Join(", ", songsList));
+                        break;
+                    default:
+                        string song = command.Substring(4);
+                        if (songsList.Contains(song))
+                        {
+                            Console.WriteLine($"{song} is already contained!");
+                        }
+                        else
+                        {
+                            songsList.Enqueue(song);
+                        }
+                        break;
+                }
+            }
+
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
