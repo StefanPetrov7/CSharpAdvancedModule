3808f6a50edd48c260a9a5028d0e531c1d6063d5
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2020-12-22 16:18:55 +0200

    Add files via upload

---
+++ StacksAndQueues/Exercise05FashionBoutique/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise05FashionBoutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> clothes = new Stack<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+
+            int rackCapacity = int.Parse(Console.ReadLine());
+            int totalRacksUsed = 1;
+            int sum = 0;
+
+            while (clothes.Count>0)
+            {
+                int curItem = clothes.Peek();
+                sum += curItem;
+
+                if (sum<=rackCapacity)
+                {
+                    clothes.Pop();
+                }
+                else
+                {
+                    totalRacksUsed++;
+                    sum = 0;
+                    sum += curItem;
+                    clothes.Pop();
+                }
+
+            }
+
+            Console.WriteLine(totalRacksUsed);
+
+        }
+    }
+}
