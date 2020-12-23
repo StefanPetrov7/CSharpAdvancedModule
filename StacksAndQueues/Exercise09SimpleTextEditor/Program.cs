ffc99e13275fa088d831ed5ea9a8ec5db2dd6542
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2020-12-23 15:32:57 +0200

    Add files via upload

---
+++ StacksAndQueues/Exercise09SimpleTextEditor/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise09SimpleTextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> textStack = new Stack<string>();
+            int interations = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < interations; i++)
+            {
+                string[] cmd = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string command = cmd[0];
+
+                switch (command)
+                {
+                    case "1":
+                        string text = cmd[1];
+                        textStack.Push(text);
+                        break;
+
+                    case "2":
+                        int charsToRemove = int.Parse(cmd[1]);
+                        text = textStack.Peek().Remove(textStack.Peek().Length - charsToRemove);
+                        textStack.Push(text);
+                        break;
+
+                    case "3":
+                        int charToPrint = int.Parse(cmd[1]) - 1;
+                        Console.WriteLine(textStack.Peek().Substring(charToPrint, 1));
+                        break;
+
+                    case "4":
+                        textStack.Pop();
+                        break;
+                }
+            }
+
+        }
+    }
+}
