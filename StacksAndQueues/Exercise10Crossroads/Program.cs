f5d4edf79d04e8a85b926f319aae56680813f339
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2020-12-23 18:25:31 +0200

    Add files via upload

---
+++ StacksAndQueues/Exercise10Crossroads/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _10._Crossroads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> cars = new Queue<string>();
+            int greenLightTime = int.Parse(Console.ReadLine());
+            int exitWindow = int.Parse(Console.ReadLine());
+            int count = 0;
+            string input;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                if (input == "green")
+                {
+                    int curTime = greenLightTime;
+                    while (cars.Count > 0)
+                    {
+                        string car = cars.Peek();
+                        int totalTime = exitWindow + curTime;
+
+                        if (car.Length <= totalTime)
+                        {
+                            curTime -= car.Length;
+                            cars.Dequeue();
+                            count++;
+                            if (curTime <= 0)
+                            {
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            string crash = car.Substring(totalTime, car.Length - totalTime);
+                            Console.WriteLine($"A crash happened!");
+                            Console.WriteLine($"{car} was hit at {crash[0]}.");
+                            return;
+                        }
+                    }
+                }
+                else
+                {
+                    cars.Enqueue(input);
+                }
+            }
+            Console.WriteLine($"Everyone is safe.");
+            Console.WriteLine($"{count} total cars passed the crossroads.");
+
+        }
+    }
+}
