e78366208e3728d1e056c79c41e183a33f7a3129
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2020-12-25 22:48:35 +0200

    Add files via upload

---
+++ Lab_02_SumMatrixColumns/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab_02_SumMatrixColumns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[,] matrix = new int[sizes[0], sizes[1]];
+
+            int[] result = new int[sizes[1]];
+
+            for (int r = 0; r < matrix.GetLength(0); r++)
+            {
+                int[] rowData = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+                for (int c = 0; c < matrix.GetLength(1); c++)
+                {
+                    matrix[r, c] = rowData[c];
+                }
+            }
+
+            for (int c = 0; c < matrix.GetLength(1); c++)
+            {
+                int sumCols = 0;
+
+                for (int r = 0; r < matrix.GetLength(0); r++)
+                {
+                    sumCols += matrix[r, c];
+                }
+
+                result[c] = sumCols;
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, result));
+
+        }
+    }
+}
