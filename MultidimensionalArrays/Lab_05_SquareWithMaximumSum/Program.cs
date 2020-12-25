55e92fa227d6ba22cac7612db6bbef98c5d368c8
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2020-12-25 22:53:19 +0200

    Add files via upload

---
+++ MultidimensionalArrays/Lab_05_SquareWithMaximumSum/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab_05_SquareWithMaximumSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[,] matrix = new int[sizes[0], sizes[1]];
+
+            int bestSquareSum = int.MinValue;
+            int bestRow = 0;
+            int bestCol = 0;
+
+            for (int r = 0; r < matrix.GetLength(0); r++)
+            {
+                int[] rowData = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int c = 0; c < matrix.GetLength(1); c++)
+                {
+                    matrix[r, c] = rowData[c];
+                }
+            }
+
+            for (int r = 0; r < matrix.GetLength(0) - 1; r++)
+            {
+                for (int c = 0; c < matrix.GetLength(1) - 1; c++)
+                {
+                    int squareSum = matrix[r, c] + matrix[r, c + 1] + matrix[r + 1, c] + matrix[r + 1, c + 1];
+
+                    if (squareSum > bestSquareSum)
+                    {
+                        bestSquareSum = squareSum;
+                        bestRow = r;
+                        bestCol = c;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{matrix[bestRow,bestCol]} {matrix[bestRow, bestCol+1]}");
+            Console.WriteLine($"{matrix[bestRow+1, bestCol]} {matrix[bestRow+1, bestCol+1]}");
+            Console.WriteLine(bestSquareSum);
+        }
+    }
+}
