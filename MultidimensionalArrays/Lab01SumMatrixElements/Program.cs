ae602de2fddee861b745f43b9530f32f00b628c2
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2020-12-25 22:47:27 +0200

    Add files via upload

---
+++ MultidimensionalArrays/Lab01SumMatrixElements/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab01SumMatrixElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int rows = sizes[0];
+            int cols = sizes[1];
+            int[,] matrix = new int[rows, cols];
+            int sum = 0;
+
+            for (int r = 0; r < matrix.GetLength(0); r++)
+            {
+                int[] rowData = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int c = 0; c < matrix.GetLength(1); c++)
+                {
+                    matrix[r, c] = rowData[c];
+                    sum += matrix[r, c];
+                }
+            }
+
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+            Console.WriteLine(sum);
+
+        }
+    }
+}
