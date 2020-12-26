11d2a9f7e557ca2d9de58705256a7e2d36f45cf4
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2020-12-26 20:16:56 +0200

    Add files via upload

---
+++ MultidimensionalArrays/Exercise_02_2X2SquaresInMatrix/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise_02_2X2SquaresInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            char[,] matrix = new char[sizes[0], sizes[1]];
+            ReadMatrix(matrix);
+
+            int equalSquares = 0;
+
+            for (int r = 0; r < matrix.GetLength(0) - 1; r++)
+            {
+                for (int c = 0; c < matrix.GetLength(1) - 1; c++)
+                {
+                    char one = matrix[r, c];
+                    char two = matrix[r, c + 1];
+                    char three = matrix[r + 1, c];
+                    char four = matrix[r + 1, c + 1];
+
+                    if ((one == two) && (two == three) && (three == four))
+                    {
+                        equalSquares++;
+                    }
+                }
+            }
+
+            Console.WriteLine(equalSquares);
+
+        }
+
+        static char[,] ReadMatrix(char[,] matrix)
+        {
+
+            for (int r = 0; r < matrix.GetLength(0); r++)
+            {
+                string[] rowData = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                for (int c = 0; c < matrix.GetLength(1); c++)
+                {
+                    matrix[r, c] = char.P(rowData[c]);
+                }
+            }
+
+            return matrix;
+
+        }
+    }
+}
