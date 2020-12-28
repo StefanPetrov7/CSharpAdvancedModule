dd9dec3db129c44bf7b6a9fd7d8cd48c84b7fa71
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2020-12-28 14:58:39 +0200

    Add files via upload

---
+++ MultidimensionalArrays/Exercise_05_01_SnakeMoves_ReverseArray/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise_05_01_SnakeMoves_ReverseArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            var snake = Console.ReadLine();
+            char[][] matrix = new char[input[0]][];
+            int startIndex = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                matrix[row] = new char[input[1]];
+                for (int col = 0; col < input[1]; col++)
+                {
+                    matrix[row][col] = snake[startIndex];
+                    startIndex++;
+
+                    if (startIndex == snake.Length)
+                    {
+                        startIndex = 0;
+                    }
+                }
+                if (row % 2 != 0)
+                {
+                    Array.Reverse(matrix[row]);
+                }
+            }
+            foreach (var row in matrix)
+            {
+                Console.WriteLine(string.Join("", row));
+            }
+        }
+    }
+}
