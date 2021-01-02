b470a9e57920b8dc7d039d203cc5564de9a45a01
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-01-02 07:58:32 +0200

    Add files via upload

---
+++ LabSetsAndDictionariesAdvanced/Exercise_02_SetsOfElements/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise_02_SetsOfElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] setLength = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            HashSet<int> firstSet = new HashSet<int>();
+            HashSet<int> secondSet = new HashSet<int>();
+
+            for (int i = 0; i < setLength[0]; i++)
+            {
+                firstSet.Add(int.Parse(Console.ReadLine()));
+            }
+
+            for (int i = 0; i < setLength[1]; i++)
+            {
+                secondSet.Add(int.Parse(Console.ReadLine()));
+            }
+
+            var unique = firstSet.Intersect(secondSet);
+
+            Console.WriteLine(string.Join(" ", unique));
+        }
+    }
+}
