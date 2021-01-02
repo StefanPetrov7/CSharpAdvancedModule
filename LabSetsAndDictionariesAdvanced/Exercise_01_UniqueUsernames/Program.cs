84f6e37de6ccc8c5f35779f074da637145416acb
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-01-02 07:39:32 +0200

    Add files via upload

---
+++ LabSetsAndDictionariesAdvanced/Exercise_01_UniqueUsernames/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise_01_UniqueUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> users = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                users.Add(Console.ReadLine());
+            }
+
+            foreach (var name in users)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
