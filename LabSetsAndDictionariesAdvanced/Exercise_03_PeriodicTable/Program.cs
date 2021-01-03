81012aa8a3fb0d314e9d921c82491091f69b60b4
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-01-03 23:05:34 +0200

    Add files via upload

---
+++ LabSetsAndDictionariesAdvanced/Exercise_03_PeriodicTable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise_03_PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            SortedSet<string> elements = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                for (int k = 0; k < input.Length; k++)
+                {
+                    elements.Add(input[k]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", elements));
+        }
+    }
+}
