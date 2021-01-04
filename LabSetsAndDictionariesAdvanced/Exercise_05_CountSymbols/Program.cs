4e926c029532d589cb9f67639da0d902a54940cb
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-01-04 14:44:22 +0200

    Add files via upload

---
+++ LabSetsAndDictionariesAdvanced/Exercise_05_CountSymbols/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Exercise_05_CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (dict.ContainsKey((char)text[i]) == false)
+                {
+                    dict.Add((char)text[i], 0);
+                }
+                dict[(char)text[i]] += 1;
+            }
+
+            foreach (var letter in dict.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{letter.Key}: {letter.Value} time/s");
+            }
+        }
+    }
+}
