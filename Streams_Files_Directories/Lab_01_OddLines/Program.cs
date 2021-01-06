e5cbbf485c447ec5574bded8060e038d9b2314ad
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-01-06 20:04:32 +0200

    Add files via upload

---
+++ Streams_Files_Directories/Lab_01_OddLines/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Lab_01_OddLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../Program.cs"))
+            {
+                string line = reader.ReadLine();
+                int index = 0;
+
+                while (line!=null)
+                {
+                    if (index%2==1)
+                    {
+                        Console.WriteLine(line);
+                    }
+                    index++;
+                    line = reader.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ Streams_Files_Directories/Lab_02_LineNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Lab_02_LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader demoFileRead = new StreamReader("../../../demoFile.txt"))
+            {
+                using (var demoFileOutput = new StreamWriter("../../../demoFileOutput.txt"))
+                {
+                    var line = demoFileRead.ReadLine();
+                    int index = 1;
+
+                    while (line != null)
+                    {
+                        demoFileOutput.WriteLine($"{index}. {line}");
+                        Console.WriteLine($"{index}. {line}");
+                        index++;
+                        line = demoFileRead.ReadLine();
+                    }
+                }
+            }
+        } 
+    }
+}
