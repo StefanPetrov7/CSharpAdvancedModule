1410ca24f76a3e2fc8a7090ad19bf8cbc9f2c536
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-01-10 00:13:51 +0200

    Add files via upload

---
+++ Streams_Files_Directories/Exercise_02_Line_Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Exercise_02_Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader read = new StreamReader("../../../text.txt"))
+            {
+                using (StreamWriter write = new StreamWriter("../../../input.txt"))
+                {
+                    Regex symbols = new Regex(@"[-\,\!\?\.\']");
+                    Regex letters = new Regex(@"[A-z]");
+                    int counter = 1;
+                    string textLine;
+
+                    while ((textLine = read.ReadLine())!=null)
+                    {
+                        int letterCount = letters.Matches(textLine).Count;
+                        int macthes = symbols.Matches(textLine).Count;
+                        write.WriteLine($"Line {counter}: {textLine}({letterCount})/({macthes})");
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
