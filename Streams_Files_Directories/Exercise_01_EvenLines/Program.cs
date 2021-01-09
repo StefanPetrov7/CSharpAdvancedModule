aee9d68b5806ab4a3d1d8e9b2ef848a2f3802375
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-01-09 23:39:54 +0200

    Add files via upload

---
+++ Streams_Files_Directories/Exercise_01_EvenLines/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Exercise_01_EvenLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex symbols = new Regex(@"[-,\.\?\!]");
+
+            using (StreamReader read = new StreamReader("../../../text.txt"))
+            {
+                using (StreamWriter write = new StreamWriter("../../../input.txt"))
+                {
+                    string line;
+                    int counter = 0;
+
+                    while ((line = read.ReadLine())!= null)
+                    {
+                        if (counter%2==0)
+                        {
+                            line = symbols.Replace(line, "@");
+                            string[] result = line.Split().ToArray().Reverse().ToArray();
+                            write.WriteLine(line);
+                        }
+                        counter++;
+
+                    }
+                }
+            }
+        }
+    }
+}
