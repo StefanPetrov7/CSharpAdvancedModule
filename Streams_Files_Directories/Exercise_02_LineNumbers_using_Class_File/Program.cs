6093ca18dd7c3d305df4dc2f2c7107576085c4c0
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-01-10 13:59:59 +0200

    Add files via upload

---
+++ Streams_Files_Directories/Exercise_02_LineNumbers_using_Class_File/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Exercise_02_LineNumbers_using_Class_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex symbols = new Regex(@"[-\,\!\?\.\']");
+            Regex letters = new Regex(@"[A-z]");
+            string[] lines = File.ReadAllLines("../../../text.txt");
+            int letterCount = 0;
+            int symbolCOunt = 0; 
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                letterCount = letters.Matches(lines[i]).Count;
+                symbolCOunt = symbols.Matches(lines[i]).Count;
+                lines[i] =  $"Line {i+1}: {lines[i]} ({letterCount})({symbolCOunt})";
+            }
+
+            File.WriteAllLines("../../../output.txt", lines);
+        }
+    }
+}
