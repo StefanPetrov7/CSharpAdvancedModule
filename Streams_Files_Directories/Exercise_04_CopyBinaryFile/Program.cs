0955d5ae2a780af3b64e598a8c6285419ac324af
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-01-10 16:39:06 +0200

    Add files via upload

---
+++ Streams_Files_Directories/Exercise_04_CopyBinaryFile/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+namespace Exercise_04_CopyBinaryFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream read = new FileStream("../../../IMG_7350.JPG", FileMode.Open))
+            {
+                using (FileStream writer = new FileStream("../../../ready_1.JPG", FileMode.CreateNew))
+                {
+                    byte[] buffer = new byte[4096];
+
+                    while (read.CanRead)
+                    {
+                        int byteRead = read.Read(buffer, 0, buffer.Length);
+
+                        if (byteRead==0)
+                        {
+                            break;
+                        }
+
+                        writer.Write(buffer, 0, buffer.Length);
+                    }
+                }
+            }
+        }
+    }
+}
