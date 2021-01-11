a1d0ed349a4d6648630a2491bb1f73d6cfd84278
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-01-12 00:05:58 +0200

    Add files via upload

---
+++ Functional_Programming/Lab_03_CountUppercaseWords/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab_03_CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<string> IsFirstLetterCapitol = str => str[0] == str.ToUpper()[0];
+
+            Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => IsFirstLetterCapitol(x))
+                .ToList()
+                .ForEach(x => Console.WriteLine(x));
+
+        }
+    }
+}
