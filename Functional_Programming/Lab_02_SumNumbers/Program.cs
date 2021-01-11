a77e54a25b753cff996e05107cfbe988122cf30f
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-01-11 17:46:46 +0200

    Add files via upload

---
+++ Functional_Programming/Lab_02_SumNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab_02_SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x))
+                .ToArray();
+            int[] result = new int[2] { nums.Length, nums.Sum() };
+
+            Console.WriteLine(string.Join(Environment.NewLine, result));
+        }
+    }
+}
