8ec0e7c6911c0723edd5f1b8fc41fbd48e1a0419
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-01-12 23:33:17 +0200

    Add files via upload

---
+++ Functional_Programming/Exercise_01_Action_Point/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercise_01_Action_Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Action<string[]> printName = x => Console.WriteLine(string.Join(Environment.NewLine, names)); 
+
+            printName(names);
+
+        }
+    }
+}
