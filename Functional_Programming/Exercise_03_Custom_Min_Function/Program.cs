dbe69093cc4c31fa421b942744b358e860412edd
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-01-13 19:38:09 +0200

    Add files via upload

---
+++ Functional_Programming/Exercise_03_Custom_Min_Function/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise_03_Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int> MyParse = x => int.Parse(x);
+
+            Func<int[], int> MinFunc = nums =>
+            {
+                int smallest = int.MaxValue;
+
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (nums[i] < smallest)
+                    {
+                        smallest = nums[i];
+                    }
+                }
+
+                return smallest;
+            };
+
+            int[] nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(MyParse)
+                .ToArray();
+
+            Action<int> Print = x => Console.WriteLine(x);
+
+            Print(MinFunc(nums));
+
+        }
+    }
+}
