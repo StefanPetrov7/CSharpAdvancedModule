9a3beaa956eabb43726aeba1ffaf24c344fd54f5
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-01-13 17:36:23 +0200

    Add files via upload

---
+++ Functional_Programming/Exercise_02_Knights_of_Honor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise_02_Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string[], string[]> AddTitle = x =>
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    input[i] = "Sir " + input[i];
+                }
+
+                return input;
+
+            };
+
+            Action<string[]> print = x => Console.WriteLine(string.Join(Environment.NewLine,input));
+
+            print(AddTitle(input));
+        }
+
+    }
+}
