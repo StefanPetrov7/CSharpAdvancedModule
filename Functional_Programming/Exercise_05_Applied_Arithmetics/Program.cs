c24f4b8f69b0aef4be664254b664b3114fe0a2fd
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-01-14 19:28:11 +0200

    Add files via upload

---
+++ Functional_Programming/Exercise_05_Applied_Arithmetics/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise_05_Applied_Arithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Action<int[]> print = nums => Console.WriteLine(string.Join(" ",nums));
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                switch (input)
+                {
+                    case "add":
+                        Operation(numbers, x => x + 1);
+                        break;
+                    case "multiply":
+                        Operation(numbers, x => x * 2);
+                        break;
+                    case "subtract":
+                        Operation(numbers, x => x - 1);
+                        break;
+                    case "print":
+                        print(numbers);
+                        break;
+                }
+            }
+        }
+
+        public static int[] Operation(int[] nums, Func<int, int> func)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = func(nums[i]);
+            }
+            return nums;
+        }
+    }
+}
