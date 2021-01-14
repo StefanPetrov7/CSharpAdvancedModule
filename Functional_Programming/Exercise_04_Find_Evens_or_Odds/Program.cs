d380410fc9baaf93ff90b4628a94b5c9a3ff5bd6
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-01-14 18:16:39 +0200

    Add files via upload

---
+++ Functional_Programming/Exercise_04_Find_Evens_or_Odds/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise_04_Find_Evens_or_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            string command = Console.ReadLine();
+
+           switch (command)
+           {
+               case "odd":
+                   GetRange(input, RemoveEven);
+                   break;
+               case "even":
+                   GetRange(input, RemoveOdd);
+                   break;
+           }
+
+          // Func<int, int, List<int>> generateList = (start, end) =>
+          // {
+          //     List<int> nums = new List<int>();
+          //
+          //     for (int i = start; i <= end; i++)
+          //     {
+          //         nums.Add(i);
+          //     }
+          //
+          //     return nums;
+          //
+          // };
+
+        }
+
+        static List<int> RemoveOdd(List<int> nums)
+        {
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (nums[i] % 2 != 0)
+                {
+                    nums.Remove(nums[i]);
+                }
+            }
+
+            return nums;
+        }
+
+        static List<int> RemoveEven(List<int> nums)
+        {
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (nums[i] % 2 == 0)
+                {
+                    nums.Remove(nums[i]);
+                }
+            }
+
+            return nums;
+        }
+
+        public static void GetRange(int[] range, Func<List<int>, List<int>> operation)
+        {
+            List<int> nums = new List<int>();
+
+            for (int i = range[0]; i <= range[1]; i++)
+            {
+                nums.Add(i);
+            }
+
+            List<int> result = operation(nums);
+            Console.WriteLine(string.Join(" ", result));
+
+        }
+    }
+}
