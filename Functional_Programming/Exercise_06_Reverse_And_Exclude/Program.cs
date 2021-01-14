b0a64e79c0b37823669c32318ef38ed8090607b8
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-01-15 00:42:24 +0200

    Add files via upload

---
+++ Functional_Programming/Exercise_06_Reverse_And_Exclude/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Exercise_06_Reverse_And_Exclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Reverse()
+                .ToArray();
+
+            int n = int.Parse(Console.ReadLine());
+
+            Predicate<int> isDevisabele = x => x % n == 0;
+
+            Func<int[], List<int>> process = nums =>
+            {
+                List<int> listNums = new List<int>();
+
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (isDevisabele(nums[i]) == false)
+                    {
+                        listNums.Add(nums[i]);
+                    }
+                }
+                return listNums;
+            };
+
+            List<int> excludedNums = process(numbers);
+
+            Console.WriteLine(string.Join(" ", excludedNums));
+
+        }
+    }
+}
