4aa5bd32540ddc6c29b7d8e4466c4dcf866e057f
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-01-15 12:22:59 +0200

    Add files via upload

---
+++ Functional_Programming/Exercise_07_Predicate_For_Names/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Exercise_07_Predicate_For_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Predicate<string> legthPredicat = x => x.Length <= n;
+
+            List<string> names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            Console.WriteLine(string.Join(Environment.NewLine, names.Where(x => legthPredicat(x))));
+
+        }
+    }
+}
