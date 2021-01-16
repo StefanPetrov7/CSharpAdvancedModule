3f0619f1a42a2efa6d2437e6e06c71df30f1dde0
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-01-16 19:18:16 +0200

    Add files via upload

---
+++ Functional_Programming/Exercise_12_TriFunction/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Exercise_12_TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string, int> charSum = name =>
+            {
+                int sum = name.ToCharArray().Sum(x => x);
+                return sum;
+            };
+
+            Func<string[], int, string> getName = (names, num) =>
+            {
+                string name = null;
+
+                for (int i = 0; i < names.Length; i++)
+                {
+                    if (charSum(names[i]) > num)
+                    {
+                        name = names[i];
+                        break;
+                    }
+                }
+                return name;
+            };
+
+            Console.WriteLine(getName(names, n)); 
+        }
+    }
+}
