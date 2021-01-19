1a7a3fb57f7c187c8ec0e6cf6f4abf8843bc9283
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-01-19 17:38:03 +0200

    Add files via upload

---
+++ Defining_Classes/Date_Modifier/DateModifier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Date_Modifier
+{
+    public static class DateModifier
+    {
+        public static double GetDaysDifference(string one, string two)
+        {
+            DateTime dayOne = DateTime.Parse(one);
+            DateTime dayTwo = DateTime.Parse(two);
+            double result = (dayTwo - dayOne).TotalDays;
+            return Math.Abs(result);
+        }
+    }
+}
---
+++ Defining_Classes/Date_Modifier/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Date_Modifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dayOne = Console.ReadLine();
+            string dayTwo = Console.ReadLine();
+            Console.WriteLine(DateModifier.GetDaysDifference(dayOne, dayTwo)); 
+        }
+    }
+}
