e157411537c149d9d1d7788c10101de87efd7190
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-01-19 17:36:08 +0200

    Add files via upload

---
+++ Defining_Classes/Exercise_01_Define_a_Class_Person/Family.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> people;
+
+        public Family()
+        {
+            this.People = new List<Person>();
+        }
+
+        public List<Person> People
+        {
+            get { return this.people; }
+            set { this.people = value; }
+        }
+
+        public void AddMember(Person person)
+        {
+            this.people.Add(person);
+        }
+
+        public Person GetOldestMember()
+        {
+            Person oldest = this.People.OrderByDescending(x => x.Age).FirstOrDefault();
+            return oldest;
+        }
+    }
+}
---
+++ Defining_Classes/Exercise_01_Define_a_Class_Person/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Family family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] info = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = info[0];
+                int age = int.Parse(info[1]);
+
+                Person newPerson = new Person(age, name);
+                family.AddMember(newPerson);
+
+            }
+
+            Person oldest = family.GetOldestMember();
+
+            Console.WriteLine(oldest.ToString());
+
+            foreach (var per in family.People.Where(x => x.Age > 30).OrderBy(x => x.Name))
+            {
+                Console.WriteLine(per.ToString());
+            }
+        }
+    }
+}
