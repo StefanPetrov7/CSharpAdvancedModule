0060c79ac18e7530b9607203862401e38e4ddb5b
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-01-21 00:37:39 +0200

    Add files via upload

---
+++ Defining_Classes/Pokemon_Trainer/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pokemon_Trainer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Trainer> trainers = new HashSet<Trainer>();
+            string input;
+
+            while ((input = Console.ReadLine()) != "Tournament")
+            {
+                string[] info = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                Pokemon pokemon = new Pokemon(info[1], info[2], int.Parse(info[3]));
+                Trainer trainer = new Trainer(info[0]);
+
+                if (trainers.Any(x => x.Name == info[0]))
+                {
+                    trainers.Where(x => x.Name == info[0]).FirstOrDefault().Pokemons.Add(pokemon);
+                }
+                else
+                {
+                    trainer.Pokemons.Add(pokemon);
+                    trainers.Add(trainer);
+                }
+
+            }
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                switch (input)
+                {
+                    case "Fire":
+                        trainers.Select(x => x.CheckPokemons("Fire")).ToHashSet();
+                        break;
+                    case "Water":
+                        trainers.Select(x => x.CheckPokemons("Water")).ToHashSet();
+                        break;
+                    case "Electricity":
+                        trainers.Select(x => x.CheckPokemons("Electricity")).ToHashSet();
+                        break;
+
+                }
+            }
+
+            trainers.OrderByDescending(x => x.Badges).ToList().ForEach(x => Console.WriteLine(x));
+
+        }
+    }
+}
---
+++ Defining_Classes/Pokemon_Trainer/Trainer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pokemon_Trainer
+{
+    public class Trainer
+    {
+        public Trainer(string name)
+        {
+            this.Name = name;
+            this.Badges = 0;
+            this.Pokemons = new List<Pokemon>();
+        }
+
+        public string Name { get; set; }
+
+        public int Badges { get; set; }
+
+        public List<Pokemon> Pokemons { get; set; }
+
+        public List<Pokemon> CheckPokemons(string input)
+        {
+
+            if (Pokemons.Exists(x => x.Element == input))
+            {
+                this.Badges += 1;
+            }
+            else
+            {
+                for (int i = 0; i < Pokemons.Count; i++)
+                {
+                    Pokemons[i].Health -= 10;
+
+                    if (Pokemons[i].Health < 1)
+                    {
+                        Pokemons.Remove(Pokemons[i]);
+                    }
+                }
+            }
+
+            return Pokemons;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Badges} {Pokemons.Count}";
+        }
+    }
+}
+
