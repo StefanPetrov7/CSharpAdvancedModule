b81f9a508e385599c53083f5d5950612fdc36797
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-01-21 18:42:43 +0200

    Add files via upload

---
+++ Archive_Exercises/Diagonal_Difference/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+
+            int primeSum = 0;
+            int secondSum = 0;
+
+            for (int r = 0; r < matrix.GetLength(0); r++)
+            {
+                int[] rowData = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int c = 0; c < matrix.GetLength(1); c++)
+                {
+                    matrix[r, c] = rowData[c];
+
+                    if (r == c)
+                    {
+                        primeSum += matrix[r, c];
+                    }
+                }
+            }
+
+            int row = 0;
+            int col = matrix.GetLength(1)-1;
+
+            for (int i = 0; i < n; i++)
+            {
+                secondSum += matrix[row, col];
+                col--;
+                row++;
+            }
+
+            Console.WriteLine(Math.Abs(primeSum-secondSum));
+        }
+    }
+}
---
+++ Archive_Exercises/Matrix_of_Palindromes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace Matrix_of_Palindromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            string[,] matrix = new string[size[0], size[1]];
+
+            char[] alpha = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+
+            for (int r = 0; r < matrix.GetLength(0); r++)
+            {
+                for (int c = 0; c < matrix.GetLength(1); c++)
+                {
+                    string one = alpha[r].ToString();
+                    string two = alpha[r + c].ToString();
+                    string three = alpha[r].ToString();
+                    string element = one + two + three;
+                    matrix[r, c] = element;
+                }
+            }
+
+            for (int r = 0; r < matrix.GetLength(0); r++)
+            {
+                for (int c = 0; c < matrix.GetLength(1); c++)
+                {
+                    Console.Write(matrix[r, c] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
