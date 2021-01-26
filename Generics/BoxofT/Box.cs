c17942c817c37c92b9d9e2c0abf5597090bab589
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-01-26 19:44:23 +0200

    Add files via upload

---
+++ Generics/BoxofT/Box.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+namespace BoxofT
+{
+    public class Box<T>
+    {
+        private Stack<T> stack;
+
+        public Box()
+        {
+            this.stack = new Stack<T>();
+        }
+
+        public int Count
+        {
+            get { return this.stack.Count; }
+        }
+
+        public void Add(T element)
+        {
+            this.stack.Push(element);
+        }
+
+        public T Remove()
+        {
+            return this.stack.Pop();
+        }
+    }
+}
---
+++ Generics/GenericArrayCreator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace GenericArrayCreator
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] array = ArrayCreator.Create(5, "Stefan");
+
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
