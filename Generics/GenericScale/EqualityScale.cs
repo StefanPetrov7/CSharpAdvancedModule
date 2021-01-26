ebdcbb46981a1135d3176b0354f031dda840d450
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-01-26 20:53:53 +0200

    Add files via upload

---
+++ Generics/GenericScale/EqualityScale.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class EqualityScale<T> 
+    {
+        private T elementOne;
+        private T elementTwo;
+
+        public EqualityScale(T elementOne, T elementTwo)
+        {
+            this.elementOne = elementOne;
+            this.elementTwo = elementTwo;
+        }
+
+        public bool AreEqual()
+        {
+            return elementOne.Equals(elementTwo);
+        }
+    }
+}
+ 
