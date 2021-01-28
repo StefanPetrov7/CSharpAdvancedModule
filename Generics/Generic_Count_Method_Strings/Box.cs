6489053ba1259ba3e7bed05468a1b697f16ae354
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-01-28 13:59:46 +0200

    Add files via upload

---
+++ Generics/Generic_Count_Method_Strings/Box.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generic_Swap_Method_Strings_Double
+{
+    public class Box<T> where T : IComparable
+    {
+        public Box(List<T> value)
+        {
+            this.Value = value;
+        }
+
+        public List<T> Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Value.GetType()}: {this.Value}";
+        }
+
+        public static void Swap<T>(List<T> list, int one, int two)
+        {
+            T elementOne = list[one];
+            list[one] = list[two];
+            list[two] = elementOne;
+        }
+
+        public int GetCountGraterElements(List<T> list, T element)
+        {
+            int result = 0;
+
+            foreach (var item in list)
+            {
+                int num = item.CompareTo(element);
+                result = num > 0 ? result += 1 : result;
+            }
+
+            return result;
+        }
+    }
+}
+
---
+++ Generics/Generic_Count_Method_Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generic_Swap_Method_Strings_Double
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<double> elements = new List<double>();
+
+            for (int i = 0; i < n; i++)
+            {
+                double input = double.Parse(Console.ReadLine());
+                elements.Add(input);
+            }
+
+            Box<double> box = new Box<double>(elements);
+            double elementToCompare = double.Parse(Console.ReadLine());
+            int result = box.GetCountGraterElements(box.Value, elementToCompare);
+            Console.WriteLine(result);
+        }
+    }
+}
