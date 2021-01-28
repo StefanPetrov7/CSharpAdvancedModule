4a0a465d3c36472c23c049575ee800c3500d5a48
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-01-28 19:32:29 +0200

    Add files via upload

---
+++ Generics/Tuple/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace Tuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nameAdress = Console.ReadLine()
+                .Split(" ")
+                .ToArray();
+            string name = nameAdress[0] + " " + nameAdress[1];
+            string street = nameAdress[2];
+            string[] cityArr = nameAdress.Skip(3).ToArray();
+            string city = $"{string.Join(" ", cityArr)}";
+
+            string[] alcoholCheck = Console.ReadLine()
+              .Split(" ")
+              .ToArray();
+            string nameTwo = alcoholCheck[0];
+            int age = int.Parse(alcoholCheck[1]);
+            string status = alcoholCheck[2] == "drunk" ? "True" : "False";
+
+            string[] bankAccount = Console.ReadLine()
+               .Split(" ")
+               .ToArray();
+            string nameThree = bankAccount[0];
+            double amount = double.Parse(bankAccount[1]);
+            string code = bankAccount[2];
+
+            Threeuple<string, string, string> lineOne
+                = new Threeuple<string, string, string>(name, street, city);
+
+            Threeuple<string, int, string> lineTWo
+                = new Threeuple<string, int, string>(nameTwo, age, status);
+
+            Threeuple<string, double, string> lineThree
+                = new Threeuple<string, double, string>(nameThree, amount, code);
+
+            Console.WriteLine(lineOne.ToString());
+            Console.WriteLine(lineTWo.ToString());
+            Console.WriteLine(lineThree.ToString());
+
+        }
+    }
+}
---
+++ Generics/Tuple/Threeuple.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tuple
+{
+    public class Threeuple<T1, T2, T3>
+    {
+        public Threeuple(T1 itemOne, T2 itemTwo, T3 itemThree)
+        {
+            this.Item1 = itemOne;
+            this.Item2 = itemTwo;
+            this.Item3 = itemThree;
+        }
+
+        public T1 Item1 { get; set; }
+
+        public T2 Item2 { get; set; }
+
+        public T3 Item3 { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Item1} -> {this.Item2} -> {this.Item3}";
+        }
+    }
+}
