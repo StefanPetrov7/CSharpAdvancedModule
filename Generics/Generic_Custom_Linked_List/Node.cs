dc1a0398bba4c885a463de9e00bcd52a20cf8930
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-01-29 20:30:32 +0200

    Add files via upload

---
+++ Generics/Generic_Custom_Linked_List/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Generic_Custom_Linked_List
+{
+    public class Node
+    {
+        public Node(int value)
+        {
+            this.Value = value;
+        }
+
+        public Node Next { get; set; }
+
+        public Node Previous { get; set; }
+
+        public int Value { get; set; }
+
+    }
+}
