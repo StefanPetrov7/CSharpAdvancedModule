ed1eae13818a98a7ea2ccb293ae66965f49ead7b
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-01-31 23:17:52 +0200

    Add files via upload

---
+++ Iterators_And_Comparators/Exercise_03_Stack/CustomStack.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Exercise_03_Stack
+{
+    public class CustomStack<T> : IEnumerable<T>
+    {
+        private T[] array;
+
+        private int count;
+
+        const int initialCApacity = 4;
+
+        public int manualCapacity;
+
+        public CustomStack()
+        {
+            array = new T[initialCApacity];
+            this.count = 0;
+        }
+
+        public CustomStack(int capacity) : this()
+        {
+            manualCapacity = capacity;
+            array = new T[manualCapacity];
+        }
+
+        public int Count => this.count;
+
+        public void Push(T element)
+        {
+            if (this.Count == array.Length)
+            {
+                Resize();
+            }
+
+            array[count] = element;
+            count++;
+        }
+
+        public T Pop()
+        {
+            if (this.count == 0)
+            {
+                throw new InvalidOperationException("No elements");
+            }
+            else
+            {
+                T element = array[--count];
+                array[count] = default;
+
+                if (this.count <= array.Length / 4)
+                {
+                    Shrink();
+                }
+
+                return element;
+            }
+
+        }
+
+        private void Shrink()
+        {
+            T[] temp = new T[array.Length / 2];
+            Array.Copy(array, temp, count);
+            array = temp;
+        }
+
+        private void Resize()
+        {
+            T[] temp = new T[array.Length * 2];
+            Array.Copy(array, temp, count);
+            array = temp;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            int iterationCount = count - 1;
+
+            while (iterationCount >= 0)
+            {
+                yield return array[iterationCount];
+                iterationCount--;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+    }
+}
---
+++ Iterators_And_Comparators/Exercise_03_Stack/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise_03_Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomStack<string> stack = new CustomStack<string>();
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] arg = input
+                    .Split(new string[] { " ", ", " }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string cmd = arg[0];
+
+                switch (cmd)
+                {
+                    case "Push":
+                        for (int i = 1; i < arg.Length; i++)
+                        {
+                            stack.Push(arg[i]);
+                        }
+                        break;
+                    case "Pop":
+                        try 
+                        {
+                            stack.Pop();
+                        }
+                        catch (InvalidOperationException e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+                }
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, stack));
+            Console.WriteLine(string.Join(Environment.NewLine, stack));
+        }
+    }
+}
