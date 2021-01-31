654f253ba62e2b02f7500c18302f6f558f5bd18d
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-01-31 23:15:45 +0200

    Add files via upload

---
+++ Iterators_And_Comparators/Exercise_Iterators_And_Comparators/ListyIterator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise_01_02_Iterators_And_Comparators
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private List<T> list;
+
+        private int index = 0;
+
+        public ListyIterator(T[] items)
+        {
+            list = items.ToList();
+        }
+
+        public bool Move()
+        {
+            index++;
+            if (list.Count <= index)
+            {
+                index--;
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool HasNext()
+        {
+            if (index == list.Count - 1)
+            {
+                return false;
+            }
+
+            return true;
+
+        }
+
+        public void Print()
+        {
+            if (list.Count == 0)
+            {
+                Console.WriteLine("Invalid Operation!");
+            }
+            else
+            {
+                Console.WriteLine($"{list[index]}");
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            int currentIndex = 0;
+
+            while (currentIndex<list.Count)
+            {
+                yield return list[currentIndex];
+                currentIndex++;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Iterators_And_Comparators/Exercise_Iterators_And_Comparators/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise_01_02_Iterators_And_Comparators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            ListyIterator<string> collection = new ListyIterator<string>(input.Skip(1).ToArray());
+
+            string cmd;
+
+            while ((cmd = Console.ReadLine()) != "END")
+            {
+                string[] arg = cmd
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                switch (arg[0])
+                {
+                    case "Move":
+                        if (collection.Move())
+                        {
+                            Console.WriteLine(true);
+                        }
+                        else
+                        {
+                            Console.WriteLine(false);
+                        }
+                        break;
+                    case "HasNext":
+                        if (collection.HasNext())
+                        {
+                            Console.WriteLine(true);
+                        }
+                        else
+                        {
+                            Console.WriteLine(false);
+                        }
+                        break;
+                    case "Print":
+                        collection.Print();
+                        break;
+                    case "PrintAll":
+                        Console.WriteLine(string.Join(" ", collection));
+                        break;
+                }
+            }
+        }
+    }
+}
