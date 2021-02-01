11f77a1555ead531b90f14ffa00cf82fd8b79cdb
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-02-01 19:55:13 +0200

    Add files via upload

---
+++ Iterators_And_Comparators/Exercise_06_Equality_Logic/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Exercise_06_Equality_Logic
+{
+    public class Person : IComparable<Person>
+    {
+        public string name;
+
+        public int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public int CompareTo(Person other)
+        {
+            int result = 1;
+
+            if (other != null)
+            {
+                result = name.CompareTo(other.name);
+
+                if (result == 0)
+                {
+                    result = age.CompareTo(other.age);
+                }
+            }
+
+            return result;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj != null && obj is Person other)
+            {
+                return this.name == other.name && this.age == other.age;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            //return this.name.GetHashCode() + this.age.GetHashCode(); // -> same result sa below code.
+            return HashCode.Combine(this.name, this.age);
+
+        }
+    }
+}
---
+++ Iterators_And_Comparators/Exercise_06_Equality_Logic/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise_06_Equality_Logic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<Person> sortedSet = new SortedSet<Person>();
+            HashSet<Person> hashset = new HashSet<Person>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] info = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = info[0];
+                int age = int.Parse(info[1]);
+
+                Person curPerson = new Person(name, age);
+                sortedSet.Add(curPerson);
+                hashset.Add(curPerson);
+            }
+
+            Console.WriteLine(sortedSet.Count);
+            Console.WriteLine(hashset.Count);
+        } 
+    }
+}
