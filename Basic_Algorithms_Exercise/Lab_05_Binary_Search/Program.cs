cfea1b84fa4751f6a95730c2f1d3e2b12ed4cbb5
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-02-03 23:48:42 +0200

    Add files via upload

---
+++ Basic_Algorithms_Exercise/Lab_05_Binary_Search/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab_05_Binary_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(BinarySearch(arr, number, 0, arr.Length)); 
+        }
+
+        private static int BinarySearch(int[] arr, int number, int start, int end)
+        {
+            if (start > end)
+            {
+                return -1;
+            }
+
+            int middle = (start + end) / 2;
+
+            if (number < arr[middle])
+            {
+                return BinarySearch(arr, number, start, middle - 1);
+            }
+            else if (number > arr[middle])
+            {
+                return BinarySearch(arr, number, middle + 1, end);
+            }
+            else
+            {
+                return middle;
+            }
+        }
+    }
+}
