9baf414cc534350bbf67f183e99efe770f5ff5aa
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-02-10 23:25:15 +0200

    Add files via upload

---
+++ CSharp_Advanced_Exams/VetClinic/Pet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace VetClinic
+{
+    public class Pet
+    {
+        private int age;
+
+        public Pet(string name, int age, string owner)
+        {
+            Name = name;
+            Age = age;
+            Owner = owner;
+        }
+
+        public string Name { get; set; }
+
+        public int Age
+        {
+            get { return this.age; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("invalide age");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public string Owner { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name} Age: {Age} Owner: {Owner}";
+        }
+    }
+}
