ca96c84789372abb660869cbf18f40fd10fc5cdf
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-02-19 15:53:17 +0200

    Add files via upload

---
+++ CSharp_Advanced_Exams/03. Openning_ Skeleton/Bakery.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace BakeryOpenning
+{
+    public class Bakery
+    {
+        private readonly int capacity;
+        private HashSet<Employee> emplyees;
+
+        public Bakery(string name, int capacity)
+        {
+            this.Name = name;
+            this.capacity = capacity;
+            this.emplyees = new HashSet<Employee>();
+        }
+
+        public string Name { get; set; }
+        public int Capacity => this.capacity;
+        public int Count => this.emplyees.Count;
+
+        public void Add(Employee employee)
+        {
+            if (this.Count < this.capacity)
+            {
+                emplyees.Add(employee);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Employee employee = emplyees.FirstOrDefault(e => e.Name == name);
+
+            if (employee != null)
+            {
+                emplyees.Remove(employee);
+                return true;
+            }
+
+            return false;
+        }
+
+        public Employee GetOldestEmployee() => emplyees.OrderByDescending(e => e.Age).FirstOrDefault();
+
+        public Employee GetEmployee(string name) => emplyees.FirstOrDefault(e => e.Name == name);
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Employees working at Bakery {this.Name}");
+            foreach (var employee in this.emplyees)
+            {
+                sb.AppendLine(employee.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
