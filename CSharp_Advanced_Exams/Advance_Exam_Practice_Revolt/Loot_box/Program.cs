508aa73dc84019e14dc97bc6b0154900f91ae5cc
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-04-12 18:25:35 +0300

    Add files via upload

---
+++ CSharp_Advanced_Exams/Advance_Exam_Practice_Revolt/Loot_box/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Loot_box
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            Stack<int> stack = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            int totalLoot = 0;
+
+            while (queue.Count > 0 && stack.Count > 0)
+            {
+                int first = queue.Peek();
+                int last = stack.Pop();
+                int sum = first + last;
+
+                if (sum % 2 == 0)
+                {
+                    totalLoot += sum;
+                    queue.Dequeue();
+                }
+                else
+                {
+                    queue.Enqueue(last);
+                }
+            }
+
+            string emptyBox = queue.Count > 0 ? "Second lootbox is empty" : "First lootbox is empty";
+            string result = totalLoot >= 100 ? $"Your loot was epic! Value: {totalLoot}" : $"Your loot was poor... Value: {totalLoot}";
+            Console.WriteLine(emptyBox);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ CSharp_Advanced_Exams/Advance_Exam_Practice_Revolt/Revolt/Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+
+namespace Revolt
+{
+    class Program
+    {
+        public class Point
+        {
+            public int Row { get; set; }
+            public int Col { get; set; }
+            public bool Finish { get; set; } = false;
+        }
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int cmdCount = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+
+            Point point = new Point();
+
+            for (int r = 0; r < matrix.GetLength(0); r++)
+            {
+                char[] rowInfo = Console.ReadLine().ToCharArray();
+
+                for (int c = 0; c < matrix.GetLength(1); c++)
+                {
+                    matrix[r, c] = rowInfo[c];
+
+                    if (matrix[r, c] == 'f')
+                    {
+                        matrix[r, c] = '-';
+                        point.Row = r;
+                        point.Col = c;
+
+                    }
+                }
+            }
+
+            for (int i = 0; i < cmdCount; i++)
+            {
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "up":
+                        MoveUp(point, matrix);
+                        break;
+                    case "down":
+                        MoveDown(point, matrix);
+                        break;
+                    case "right":
+                        MoveRight(point, matrix);
+                        break;
+                    case "left":
+                        MoveLeft(point, matrix);
+                        break;
+                }
+
+                if (point.Finish == true)
+                {
+                    break;
+                }
+            }
+
+            string result = point.Finish ? "Player won!" : "Player lost!";
+            matrix[point.Row, point.Col] = 'f';
+            Console.WriteLine(result);
+            PrintMatrix(matrix);
+        }
+
+        private static void MoveLeft(Point point, char[,] matrix)
+        {
+            point.Col -= 1;
+            CheckPossitionIfOut(point, matrix);
+
+            if (matrix[point.Row, point.Col] == 'B')
+            {
+                point.Col -= 1;
+                CheckPossitionIfOut(point, matrix);
+            }
+
+            if (matrix[point.Row, point.Col] == 'T')
+            {
+                point.Col += 1;
+                CheckPossitionIfOut(point, matrix);
+            }
+
+            IsFinished(point, matrix);
+        }
+
+        private static void MoveRight(Point point, char[,] matrix)
+        {
+            point.Col += 1;
+            CheckPossitionIfOut(point, matrix);
+
+            if (matrix[point.Row, point.Col] == 'B')
+            {
+                point.Col += 1;
+                CheckPossitionIfOut(point, matrix);
+            }
+
+            if (matrix[point.Row, point.Col] == 'T')
+            {
+                point.Col -= 1;
+                CheckPossitionIfOut(point, matrix);
+            }
+
+            IsFinished(point, matrix);
+        }
+
+        private static void MoveDown(Point point, char[,] matrix)
+        {
+            point.Row += 1;
+
+            CheckPossitionIfOut(point, matrix);
+
+            if (matrix[point.Row, point.Col] == 'B')
+            {
+                point.Row += 1;
+                CheckPossitionIfOut(point, matrix);
+            }
+
+            if (matrix[point.Row, point.Col] == 'T')
+            {
+                point.Row -= 1;
+                CheckPossitionIfOut(point, matrix);
+            }
+
+            IsFinished(point, matrix);
+        }
+
+
+        private static void MoveUp(Point point, char[,] matrix)
+        {
+            point.Row -= 1;
+
+            CheckPossitionIfOut(point, matrix);
+
+            if (matrix[point.Row, point.Col] == 'B')
+            {
+                point.Row -= 1;
+                CheckPossitionIfOut(point, matrix);
+            }
+
+            if (matrix[point.Row, point.Col] == 'T')
+            {
+                point.Row += 1;
+                CheckPossitionIfOut(point, matrix);
+            }
+
+            IsFinished(point, matrix);
+        }
+
+        private static void CheckPossitionIfOut(Point point, char[,] matrix)
+        {
+            if (point.Row < 0)
+            {
+                point.Row = matrix.GetLength(0) - 1;
+            }
+            else if (point.Row > matrix.GetLength(0) - 1)
+            {
+                point.Row = 0;
+            }
+            else if (point.Col < 0)
+            {
+                point.Col = matrix.GetLength(1) - 1;
+            }
+            else if (point.Col > matrix.GetLength(1) - 1)
+            {
+                point.Col = 0;
+            }
+        }
+
+        private static void IsFinished(Point point, char[,] matrix)
+        {
+            if (matrix[point.Row, point.Col] == 'F')
+            {
+                point.Finish = true;
+            }
+        }
+
+        private static void PrintMatrix(char[,] matrix)
+        {
+            for (int r = 0; r < matrix.GetLength(0); r++)
+            {
+                for (int c = 0; c < matrix.GetLength(1); c++)
+                {
+                    Console.Write(matrix[r, c]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
