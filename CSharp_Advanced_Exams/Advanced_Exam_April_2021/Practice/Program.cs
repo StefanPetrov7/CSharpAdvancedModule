27850fbef01f1a3412bd7672083b2a4c05b3ab55
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-04-14 16:43:52 +0300

    Add files via upload

---
+++ CSharp_Advanced_Exams/Advanced_Exam_April_2021/Practice/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+
+namespace Super_Mario
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int health = int.Parse(Console.ReadLine());
+            byte size = byte.Parse(Console.ReadLine());
+
+            int[] marioHealth = new int[] { health };
+            int[] marioRow = new int[1];
+            int[] marioCol = new int[1];
+
+            char[][] matrix = new char[size][];
+
+            ComleteMatrix(matrix, size, marioRow, marioCol);
+
+            while (true)
+            {
+                string[] arg = Console.ReadLine().Split();
+
+                string move = arg[0];
+                int enemyRow = int.Parse(arg[1]);
+                int enemyCol = int.Parse(arg[2]);
+
+                matrix[marioRow[0]][marioCol[0]] = '-';
+
+                SpawnEnemy(enemyRow, enemyCol, matrix);
+
+                switch (move)
+                {
+                    case "W":
+                        marioRow[0]--;
+                        marioHealth[0] -= 1;
+                        CheckIfMarioIsOut(marioRow, marioCol, marioHealth, matrix);
+                        InspectCrll(marioRow, marioCol, marioHealth, matrix);
+                        break;
+                    case "S":
+                        marioRow[0]++;
+                        marioHealth[0] -= 1;
+                        CheckIfMarioIsOut(marioRow, marioCol, marioHealth, matrix);
+                        InspectCrll(marioRow, marioCol, marioHealth, matrix);
+                        break;
+                    case "A":
+                        marioCol[0]--;
+                        marioHealth[0] -= 1;
+                        CheckIfMarioIsOut(marioRow, marioCol, marioHealth, matrix);
+                        InspectCrll(marioRow, marioCol, marioHealth, matrix);
+                        break;
+                    case "D":
+                        marioCol[0]++;
+                        marioHealth[0] -= 1;
+                        CheckIfMarioIsOut(marioRow, marioCol, marioHealth, matrix);
+                        InspectCrll(marioRow, marioCol, marioHealth, matrix);
+                        break;
+                }
+
+                if (matrix[marioRow[0]][marioCol[0]] == 'P')
+                {
+                    matrix[marioRow[0]][marioCol[0]] = '-';
+                    Console.WriteLine($"Mario has successfully saved the princess! Lives left: {marioHealth[0]}");
+                    break;
+                }
+
+                if (marioHealth[0] <= 0)
+                {
+                    matrix[marioRow[0]][marioCol[0]] = 'X';
+                    Console.WriteLine($"Mario died at {marioRow[0]};{marioCol[0]}.");
+                    break;
+                }
+            }
+
+            PrintMatrix(matrix);
+        }
+
+        private static void InspectCrll(int[] marioRow, int[] marioCol, int[] marioHealth, char[][] matrix)
+        {
+
+            if (matrix[marioRow[0]][marioCol[0]] == 'B')
+            {
+                marioHealth[0] -= 2;
+            }
+        }
+
+        private static void CheckIfMarioIsOut(int[] marioRow, int[] marioCol, int[] marioHealth, char[][] matrix)
+        {
+            if (marioRow[0] < 0)
+            {
+                marioRow[0] = 0;
+            }
+
+            if (marioRow[0] > matrix.Length - 1)
+            {
+                marioRow[0] = matrix.Length - 1;
+            }
+
+            if (marioCol[0] < 0)
+            {
+                marioCol[0] = 0;
+            }
+
+            if (marioCol[0] > matrix[marioRow[0]].Length - 1)
+            {
+                marioCol[0] = matrix[marioRow[0]].Length - 1;
+            }
+
+        }
+
+        private static void SpawnEnemy(int enemyRow, int enemyCol, char[][] matrix)
+        {
+            matrix[enemyRow][enemyCol] = 'B';
+        }
+
+        private static void PrintMatrix(char[][] matrix)
+        {
+            for (int r = 0; r < matrix.Length; r++)
+            {
+                for (int c = 0; c < matrix[r].Length; c++)
+                {
+                    Console.Write(matrix[r][c]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static void ComleteMatrix(char[][] matrix, int size, int[] marioRow, int[] marioCol)
+        {
+            for (int r = 0; r < size; r++)
+            {
+                char[] rowInfo = Console.ReadLine().ToCharArray();
+
+                matrix[r] = new char[rowInfo.Length];
+
+                for (int c = 0; c < rowInfo.Length; c++)
+                {
+                    matrix[r][c] = rowInfo[c];
+
+                    if (matrix[r][c] == 'M')
+                    {
+                        marioRow[0] = r;
+                        marioCol[0] = c;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CSharp_Advanced_Exams/Advanced_Exam_April_2021/Super_Mario/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+
+namespace Mario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int marioLives = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, 20];
+            int playerRow = 0;
+            int playerCol = 0;
+            bool IsSaved = false;
+
+            for (int i = 0; i < n; i++)
+            {
+                string currentRow = Console.ReadLine();
+                for (int j = 0; j < currentRow.Length; j++)
+                {
+                    matrix[i, j] = currentRow[j];
+
+                    if (matrix[i, j] == 'M')
+                    {
+                        playerRow = i;
+                        playerCol = j;
+                    }
+
+                }
+            }
+
+
+            while (true)
+            {
+                if (marioLives <= 0)
+                {
+                    matrix[playerRow, playerCol] = 'X';
+                    break;
+                }
+
+                string[] line = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string command = line[0];
+                int bowserRow = int.Parse(line[1]);
+                int bowserCol = int.Parse(line[2]);
+                matrix[bowserRow, bowserCol] = 'B';
+
+                matrix[playerRow, playerCol] = '-';
+                marioLives--;
+
+                if (IsOut(command, playerRow, playerCol, n))
+                {
+                    continue;
+                }
+
+                playerRow = MoveRow(command, playerRow);
+                playerCol = MoveCol(command, playerCol);
+
+                if (matrix[playerRow, playerCol] == 'B')
+                {
+                    marioLives -= 2;
+                    if (marioLives <= 0)
+                    {
+                        matrix[playerRow, playerCol] = 'X';
+                        break;
+
+                    }
+                    else
+                    {
+                        matrix[playerRow, playerCol] = '-';
+                    }
+                }
+                else if (matrix[playerRow, playerCol] == 'P')
+                {
+                    IsSaved = true;
+                    matrix[playerRow, playerCol] = '-';
+                    break;
+                }
+
+                matrix[playerRow, playerCol] = 'M';
+            }
+
+            if (IsSaved == true)
+            {
+                Console.WriteLine($"Mario has successfully saved the princess! Lives left: {marioLives}");
+            }
+            else
+            {
+                Console.WriteLine($"Mario died at {playerRow};{playerCol}.");
+            }
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"{matrix[row, col]}");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+
+        private static int MoveCol(string command, int position)
+        {
+            switch (command)
+            {
+                case "A": return --position;
+                case "D": return ++position;
+                default: return position;
+            }
+        }
+
+        private static int MoveRow(string command, int position)
+        {
+            switch (command)
+            {
+                case "W": return --position;
+                case "S": return ++position;
+
+                default: return position;
+            }
+        }
+
+        private static bool IsOut(string command, int playerRow, int playerCol, int n)
+        {
+            switch (command)
+            {
+                case "W": return playerRow - 1 < 0;
+                case "S": return playerRow + 1 >= n;
+                case "A": return playerCol - 1 < 0;
+                case "D": return playerCol + 1 >= n;
+                default: return false;
+
+            }
+        }
+    }
+}
