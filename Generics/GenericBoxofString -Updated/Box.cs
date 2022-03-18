459203d672324fcb394234a7b113169c035138e2
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2022-03-18 17:02:08 +0200

    Add files via upload

---
+++ Generics/GenericBoxofString -Updated/Box.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxofString
+{
+    public class Box<T>
+    {
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; private set; }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine($"{this.Value.GetType().FullName}: {this.Value}");
+            return result.ToString().TrimEnd();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ Generics/GenericBoxofString -Updated/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericBoxofString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Box<int>> boxes = new List<Box<int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                Box<int> box = new Box<int>(int.Parse(Console.ReadLine()));
+                boxes.Add(box);
+            }
+
+            int[] indexes = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Swab(boxes, indexes[0], indexes[1]);
+
+            boxes.ForEach(x => Console.WriteLine(x.ToString()));
+        }
+
+
+        public static void Swab<T>(List<Box<T>> list, int indexOne, int indexTwo)
+        {
+            Box<T> elementOne = list[indexOne];
+            Box<T> elementTwo = list[indexTwo];
+            list[indexOne] = elementTwo;
+            list[indexTwo] = elementOne;
+        }
+    }
+}
