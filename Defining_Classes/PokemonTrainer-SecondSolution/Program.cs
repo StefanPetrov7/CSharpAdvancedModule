e236b6a04bda3193c7d9c264effb0e4665c19ede
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2022-03-19 15:46:49 +0200

    Add files via upload

---
+++ Defining_Classes/PokemonTrainer-SecondSolution/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PokemonTrainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Trainer> trainerLog = new Dictionary<string, Trainer>();
+            string input;
+
+            while ((input = Console.ReadLine()) != "Tournament")
+            {
+                var arg = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string trainerName = arg[0];
+                string pokemonName = arg[1];
+                string pokemonElement = arg[2];
+                int pokemonHealth = int.Parse(arg[3]);
+
+                Pokemon pokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
+                Trainer trainer = trainerLog.FirstOrDefault(x => x.Key == trainerName).Value;
+
+                if (trainer == null)
+                {
+                    trainer = new Trainer(trainerName);
+                    trainerLog.Add(trainerName, trainer);
+                }
+
+                trainer.Pokemons.Add(pokemon);
+
+            }
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string cmd = input;
+
+                switch (cmd)
+                {
+                    case "Fire":
+                        Action(trainerLog, cmd);
+                        break;
+                    case "Water":
+                        Action(trainerLog, cmd);
+                        break;
+                    case "Electricity":
+                        Action(trainerLog, cmd);
+                        break;
+                }
+            }
+
+            foreach (var trainer in trainerLog.OrderByDescending(x => x.Value.NumberOfBadges))
+            {
+                Console.WriteLine(trainer.Value);
+            }
+
+        }
+
+        public static void Action(Dictionary<string, Trainer> log, string cmd)
+        {
+            foreach (var kvp in log)
+            {
+                Trainer trainer = kvp.Value;
+
+                if (trainer.Pokemons.Any(x => x.Element == cmd))
+                {
+                    trainer.NumberOfBadges += 1;
+                }
+                else
+                {
+                    trainer.Pokemons.ToList().ForEach(x => x.Health -= 10);
+                }
+
+                trainer.RemoveDeadPokemons();
+            }
+        }
+    }
+}
---
+++ Defining_Classes/PokemonTrainer-SecondSolution/Trainer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PokemonTrainer
+{
+    public class Trainer
+    {
+        public Trainer(string name)
+        {
+            this.Name = name;
+            this.NumberOfBadges = 0;
+            this.Pokemons = new HashSet<Pokemon>();
+        }
+
+        public string Name { get; set; }
+
+        public int NumberOfBadges { get; set; }
+
+        public HashSet<Pokemon> Pokemons { get; set; }
+
+        public void RemoveDeadPokemons()
+        {
+            this.Pokemons = this.Pokemons.Where(x => x.Health > 0).ToHashSet();
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name} {this.NumberOfBadges} {this.Pokemons.Count}";
+        }
+    }
+}
