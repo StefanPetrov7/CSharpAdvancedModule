117eab1eb742da9c8ec9563fb1c3628ea62b6ac1
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2023-09-03 20:06:45 +0300

    Add files via upload

---
+++ Practice_2023/Matching_Brackets_Updated.cs
@@ -0,0 +1,28 @@
+﻿namespace Practice_2023;
+
+class Matching_Brackets_Updated
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        Stack<int> opendBrackets = new Stack<int>();
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            string curElement = input[i].ToString();
+
+            if (curElement == "(")
+            {
+                opendBrackets.Push(i);
+                continue;
+            }
+
+            if (curElement == ")")
+            {
+                int startIndex = opendBrackets.Pop();
+                Console.WriteLine(input.Substring(startIndex, i - startIndex + 1));
+                continue;
+            }
+        }
+    }
+}
