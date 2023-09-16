c4edb72929d0243ed02fafef4babf430463e4883
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2023-09-16 19:40:25 +0300

    Add files via upload

---
+++ Practice_2023/StartUp_BoxOfT.cs
@@ -0,0 +1,40 @@
+﻿namespace BoxOfT;
+
+class StartUp
+{
+    static void Main(string[] args)
+    {
+
+        Box<int> box = new Box<int>();
+        box.Add(1);
+        box.Add(2);
+        box.Add(3);
+        Console.WriteLine(box.Remove());
+        Console.WriteLine(box.Count);
+
+    }
+
+    public class Box<T>
+    {
+        private Stack<T> internalStack;
+
+        public Box()
+        {
+            this.internalStack = new Stack<T>();
+        }
+
+        public void Add(T element)
+        {
+            this.internalStack.Push(element);
+        }
+
+        public T Remove()
+        {
+            T element = this.internalStack.Pop();
+            return element;
+        }
+
+        public int Count => this.internalStack.Count;
+
+    }
+}
