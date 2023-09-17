bf8d9d243156f09304c344095da567f27acc4fd0
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2023-09-17 12:48:41 +0300

    Add files via upload

---
+++ Practice_2023/StartUp_Scale.cs
@@ -0,0 +1,31 @@
+﻿namespace GenericScale;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+
+        EqualityScale<int> scale = new EqualityScale<int>(2, 2);
+        Console.WriteLine(scale.AreEqual());
+
+    }
+
+    public class EqualityScale<T>
+    {
+        private T element1;
+
+        private T element2;
+
+        public EqualityScale(T element1, T element2)
+        {
+            this.element1 = element1;
+            this.element2 = element2;
+        }
+
+        public bool AreEqual()
+        {
+            return this.element1.Equals(this.element2);
+
+        }
+    }
+}
