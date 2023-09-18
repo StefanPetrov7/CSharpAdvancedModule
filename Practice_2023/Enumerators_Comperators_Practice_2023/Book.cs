cc2148c27985a117193d95c3889ea48b7d61c299
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2023-09-18 18:19:08 +0300

    Add files via upload

---
+++ Practice_2023/Enumerators_Comperators_Practice_2023/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace IteratorsAndComparators
+{
+	public class Book
+	{
+		public Book(string title, int year, params string[]  authors)
+		{
+			this.Title = title;
+			this.Year = year;
+			this.Authors = authors.ToList();
+		}
+
+		public string Title { get; set; }
+
+		public int Year { get; set; }
+
+		public List<string> Authors { get; set; }
+	}
+}
+
---
+++ Practice_2023/Enumerators_Comperators_Practice_2023/Library.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>  // Implementing Enumerable collection with private Iterator 
+    {
+        public Library(params Book[] books)
+        {
+            this.Books = books.ToList();
+        }
+
+        public List<Book> Books { get; set; }
+
+        public IEnumerator<Book> GetEnumerator()  // return Enumerator implemented with LibraryIterator 
+        {
+            return new LibraryIterator(this.Books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+
+        private class LibraryIterator : IEnumerator<Book>  // Implementing enumerator
+        {
+            private int index = -1; // starting index
+
+            public LibraryIterator(IEnumerable<Book> books)  // reqieres collection 
+            {
+                this.Reset();
+                this.Books = new List<Book>(books);
+            }
+
+            public List<Book> Books { get; set; }
+
+            public Book Current => this.Books[index];  // return current index 
+
+            object IEnumerator.Current => this.Current;  // old implementation 
+
+            public void Dispose() { } 
+
+            public bool MoveNext() => ++index < this.Books.Count;  // this is called in the cycles and unil is true index is ++
+  
+            public void Reset()  
+            {
+                this.index = -1;
+            }
+        }
+    }
+}
