f0e4b00367bca7b72d24e9983f88f398438417b2
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2023-09-20 20:19:23 +0300

    Add files via upload

---
+++ Practice_2023/Enumerators_Comperators_Practice_2023/Froggy/Lake.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+
+namespace IteratorsAndComparators
+{
+    public class Lake<T> : IEnumerable<T>
+    {
+        private readonly T[] arr;
+
+        public Lake(params T[] values)
+        {
+            this.arr = values.ToArray();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < this.arr.Length; i += 2)
+            {
+                yield return this.arr[i];
+            }
+
+            for (int i = this.arr.Length - 1; i > 0; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    yield return this.arr[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
+
---
+++ Practice_2023/Enumerators_Comperators_Practice_2023/Froggy/StartUp.cs
@@ -0,0 +1,10 @@
+﻿namespace IteratorsAndComparators;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+        Lake<int> lake = new Lake<int>(input);
+        Console.WriteLine(string.Join(", ", lake));
+    }
+}
