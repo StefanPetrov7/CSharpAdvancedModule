41a2132b5cc00a9c2dae2dd259a74e4948bb677b
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2023-09-21 17:44:08 +0300

    Add files via upload

---
+++ Practice_2023/Enumerators_Comperators_Practice_2023/EqualityLogic/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public int CompareTo(Person person)
+        {
+            if (this.Name.CompareTo(person.Name) == 0)
+            {
+                return this.Age.CompareTo(person.Age);
+            }
+            else
+            {
+                return this.Name.CompareTo(person.Name);
+            }
+        }
+
+        // below two methods need to be overrided in order the sets to implement different obj comparing 
+
+        public override int GetHashCode()  // override the method so the objects can be compared as per our parameters 
+        {
+            return this.Name.GetHashCode() + this.Age.GetHashCode();
+        }
+
+        public override bool Equals(Object obj) // override the method so the objects can be compared as per our parameters 
+        {
+            if (obj != null && obj is Person person)
+            {
+                return this.Name == person.Name && this.Age == person.Age;
+            }
+
+            return false;
+        }
+    }
+}
+
---
+++ Practice_2023/Enumerators_Comperators_Practice_2023/EqualityLogic/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace EqualityLogic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HashSet<Person> hashPeaople = new HashSet<Person>();
+        SortedSet<Person> sortSetPeaople = new SortedSet<Person>();
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] arg = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            hashPeaople.Add(new Person(arg[0], int.Parse(arg[1])));
+            sortSetPeaople.Add(new Person(arg[0], int.Parse(arg[1])));
+        }
+
+        Console.WriteLine(sortSetPeaople.Count);
+        Console.WriteLine(hashPeaople.Count);
+
+    }
+}
