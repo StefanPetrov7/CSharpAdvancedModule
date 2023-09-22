6a57242cd49b8770a9d2fbf15c0965e385391ad9
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2023-09-22 13:48:02 +0300

    Add files via upload

---
+++ Practice_2023/DateModifier/DateModifier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace DateModifier
+{
+	public class DateModifier
+	{
+		public DateModifier() { }
+
+		public static TimeSpan CalculateDateDifference(int[] date1, int[] date2)
+		{
+			
+			DateTime firstDate = new DateTime(date1[0], date1[1], date1[2]);
+            DateTime secondDate = new DateTime(date2[0], date2[1], date2[2]);
+			TimeSpan differenceInDays = firstDate - secondDate;
+			return differenceInDays;
+        }
+    }
+}
+
---
+++ Practice_2023/DateModifier/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DateModifier;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int[] dateOne = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        int[] dateTwo = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        TimeSpan result = DateModifier.CalculateDateDifference(dateOne, dateTwo);
+        int days = Math.Abs(int.Parse(result.Days.ToString()));
+        Console.WriteLine(days);
+    }
+}
+
