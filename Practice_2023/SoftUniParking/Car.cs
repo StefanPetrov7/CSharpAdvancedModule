5a4c849fb9a16ec558bb6313c60b95251cde9945
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2023-09-23 17:07:41 +0300

    Add files via upload

---
+++ Practice_2023/SoftUniParking/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace SoftUniParking
+{
+    public class Car
+    {
+        public Car(string make, string model, int hp, string regNumber)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.HorsePower = hp;
+            this.RegistrationNumber = regNumber;
+        }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int HorsePower { get; set; }
+
+        public string RegistrationNumber { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder text = new StringBuilder();
+            text.AppendLine($"Make: {this.Make}");
+            text.AppendLine($"Model: {this.Model}");
+            text.AppendLine($"HorsePower: {this.HorsePower}");
+            text.AppendLine($"RegistrationNumber: {this.RegistrationNumber}");
+            return text.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Practice_2023/SoftUniParking/Parking.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+        private HashSet<Car> parking;
+        private int capacity;
+
+        public Parking(int capacity)
+        {
+            this.capacity = capacity;
+            this.parking = new HashSet<Car>(this.capacity);
+        }
+
+        public int Count => this.parking.Count;
+
+        public string AddCar(Car car)
+        {
+            if (this.parking.Any(x => x.RegistrationNumber == car.RegistrationNumber))
+            {
+                return "Car with that registration number, already exists!";
+            }
+
+            if (this.parking.Count == this.capacity)
+            {
+                return "Parking is full!";
+            }
+
+            this.parking.Add(car);
+            return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+        }
+
+        public string RemoveCar(string RegistrationNumber)
+        {
+            Car car = this.parking.FirstOrDefault(x => x.RegistrationNumber == RegistrationNumber);
+
+            if (car == null)
+            {
+                return $"Car with that registration number, doesn't exist!";
+            }
+
+            this.parking.Remove(car);
+            return $"Successfully removed {RegistrationNumber}";
+
+        }
+
+        public Car GetCar(string RegistrationNumber) => this.parking.FirstOrDefault(x => x.RegistrationNumber == RegistrationNumber);
+
+        public void RemoveSetOfRegistrationNumber(List<string> regNumbers)
+        {
+            foreach (var number in regNumbers)
+            {
+                Car car = this.parking.FirstOrDefault(x => x.RegistrationNumber == number);
+                this.parking.Remove(car);
+            }
+        }
+    }
+}
+
