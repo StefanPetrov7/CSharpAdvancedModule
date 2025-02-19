f3ffa94a9f65ba8a27517a707dc4581af08c0197
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2025-02-19 17:01:12 +0200

    Add files via upload

---
+++ AdvancePractice2025/Exam22June2025/RapidCourier.cs
@@ -0,0 +1,50 @@
+﻿namespace ExamJune22
+{
+    internal class RapidCourier
+    {
+        static void Main(string[] args)
+        {
+            var packages = new Stack<int>(Console.ReadLine().Split(" ").Select(int.Parse));
+            var couriers = new Queue<int>(Console.ReadLine().Split(" ").Select(int.Parse));
+            int totalDelivery = 0;
+
+            while (packages.Count > 0 && couriers.Count > 0)
+            {
+                int deliveringCapacity = couriers.Dequeue();
+                int curPackage = packages.Pop();
+
+                if (deliveringCapacity >= curPackage) 
+                {
+
+                    if (deliveringCapacity > (curPackage * 2))
+                    {
+                        int remainingCapacity = deliveringCapacity - (curPackage * 2);
+                        couriers.Enqueue(remainingCapacity);
+                    }
+
+                    totalDelivery += curPackage;
+                }
+                else
+                {
+                    packages.Push(curPackage - deliveringCapacity);
+                    totalDelivery += deliveringCapacity;
+                }
+            }
+
+            Console.WriteLine($"Total weight: {totalDelivery} kg");
+
+            if (packages.Count == 0 && couriers.Count == 0)
+            {
+                Console.WriteLine($"Congratulations, all packages were delivered successfully by the couriers today.");
+            }
+            else if (packages.Count > 0 && couriers.Count == 0)
+            {
+                Console.WriteLine($"Unfortunately, there are no more available couriers to deliver the following packages: {String.Join(", ", packages)}");
+            }
+            else
+            {
+                Console.WriteLine($"Couriers are still on duty: {String.Join(", ", couriers)} but there are no more packages to deliver.");
+            }
+        }
+    }
+}
