31b2b4de7205b41e1c9672dbb797cd01a172cee5
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2025-02-21 01:05:46 +0200

    Add files via upload

---
+++ AdvancePractice2025/Exam22June2025/EstateAgency/EstateAgency.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+
+namespace EstateAgency
+{
+    public class EstateAgency
+    {
+        public EstateAgency(int capacity)
+        {
+            this.Capacity = capacity;
+            this.RealEstates = new List<RealEstate>();
+        }
+
+        public int Capacity { get; set; }
+
+        public List<RealEstate> RealEstates { get; set; }
+
+        public int Count => this.RealEstates.Count;
+
+        public void AddRealEstate(RealEstate realEstate)
+        {
+            if (this.RealEstates.Count == this.Capacity)
+                return;
+            if (this.RealEstates.Any(x => x.Address == realEstate.Address))
+                return;
+
+            this.RealEstates.Add(realEstate);
+        }
+
+        public bool RemoveRealEstate(string address)
+        {
+            var estate = this.RealEstates.FirstOrDefault(x => x.Address == address);
+            return this.RealEstates.Remove(estate);
+        }
+
+        public List<RealEstate> GetRealEstates(string postalCode)
+        {
+            var result = new List<RealEstate>();
+
+            result = this.RealEstates.Where(x => x.PostalCode == postalCode).ToList();
+
+            return result;
+        }
+
+        public RealEstate GetCheapest() => this.RealEstates.OrderBy(x => x.Price).First();
+
+        public int GetLargest() => (int)this.RealEstates.OrderByDescending(x => x.Size).First().Size;
+
+        public string EstateReport()
+        {
+            StringBuilder result = new StringBuilder();
+
+            result.AppendLine("Real estates available:");
+
+            foreach (var estate in this.RealEstates)
+            {
+                result.AppendLine(estate.ToString());
+            }
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
---
+++ AdvancePractice2025/Exam22June2025/EstateAgency/RealEstate.cs
@@ -0,0 +1,26 @@
+﻿namespace EstateAgency
+{
+    public class RealEstate
+    {
+        public RealEstate(string address, string postalCode, decimal price, double size)
+        {
+            this .Address = address;    
+            this .PostalCode = postalCode;  
+            this .Price = price;    
+            this .Size = size;  
+        }
+
+        public string Address  { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public decimal Price { get; set; }
+
+        public double Size { get; set; }
+
+        public override string ToString()
+        {
+            return $"Address: {this.Address}, PostalCode: {this.PostalCode}, Price: ${this.Price}, Size: {this.Size} sq.m.";
+        }
+    }
+}
